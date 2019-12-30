1a24b8cfe351fc5ff61f4921d7ccbdca49a257d1
Diego <diego.gomezur@elorrieta-errekamari.com>
2019-12-30 18:11:16 +0100

    hlaa

---
+++ app/src/main/java/com/turismoalojamenduakandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.turismoalojamenduakandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //pruebaa
+    }
+}
